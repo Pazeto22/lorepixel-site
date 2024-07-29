d6d32d71a51bfea324f05e078bb787ce3387d29e
Gustavo Pazeto <gustavopazeto95@gmail.com>
2024-07-29 01:12:59 -0300

    refactor: Novo projeto (create vue@latest)

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,14 @@
-import { createApp } from 'vue';
-import { createPinia } from 'pinia';
-import '@/style.scss';
-import App from '@/App.vue';
-import router from '@/router';
+import './assets/main.css'
 
-const app = createApp(App);
+import { createApp } from 'vue'
+import { createPinia } from 'pinia'
 
-app.use(createPinia());
-app.use(router);
-app.mount('#app');
+import App from './App.vue'
+import router from './router'
+
+const app = createApp(App)
+
+app.use(createPinia())
+app.use(router)
+
+app.mount('#app')
--- src/pages/home/home.vue
+++
@@ -1,8 +0,0 @@
-<template>
-  <HelloWorld />
-  <div>Teste</div>
-</template>
-
-<script setup>
-  import HelloWorld from '@/components/HelloWorld.vue';
-</script>
